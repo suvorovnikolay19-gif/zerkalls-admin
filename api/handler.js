71828c2dadf9c062a273497c8ad7146734ad60ec
suvorovnikolay19-gif <niksuv629@gmail.com>
2026-08-20 10:31:44 +0300

    fix: explicit CORS headers in serverless handler

--- api/handler.js
+++ api/handler.js
@@ -20,6 +20,15 @@ app.use('/api/mirror-classes', mirrorClassesRouter);
 let initialized = false;
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (!initialized) {
     await initDb();
     initialized = true;
